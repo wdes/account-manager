c3ffd12d1baf13f6101d1d0547e602694980af07
William Desportes <williamdes@wdes.fr>
2018-05-18 23:44:16 +0200

    Modified generate_pot & po_update

--- scripts/generate_pot.php
+++ scripts/generate_pot.php
@@ -1,37 +1,72 @@
 #!/usr/bin/php
 <?php
-$path = realpath('./locale');
-bindtextdomain($domain, $path);
-bind_textdomain_codeset($domain, "UTF-8");
-textdomain($domain);
+/**
+ * @license http://unlicense.org/UNLICENSE The UNLICENSE
+ * @author William Desportes <williamdes@wdes.fr>
+ */
 require_once __DIR__.'/../vendor/autoload.php';
-$tplDir = __DIR__.'/../src/templates';
-$tmpDir = __DIR__.'/../tmp/twig_cache/';
+$templatesPath = "src/";
+$tplDir = realpath(__DIR__.'/../'.$templatesPath.'templates');
+$tmpDir = realpath(__DIR__.'/../tmp/twig_cache/').'/';
 $loader = new Twig_Loader_Filesystem($tplDir);
-
+$cacheFS = new \Twig_Cache_Filesystem($tmpDir);
+PhpMyAdmin\MoTranslator\Loader::loadFunctions();
 // force auto-reload to always have the latest version of the template
 $twig = new Twig_Environment($loader, array(
-    'cache' => $tmpDir,
+    'cache' => $cacheFS,//$tmpDir,
     'auto_reload' => true
 ));
-$twig->addExtension(new Twig_Extensions_Extension_I18n());
-// configure Twig the way you want
+$twig->addExtension(new AccountManager\Twig\I18nExtension());
+
+$mappings = new stdClass();
+$mappings->mappings = array();
+$mappings->replacements = array();
+$description = new stdClass();
+$description->from = "SOME DESCRIPTIVE TITLE";
+$description->to = "Wdes Account manager translation";
+$mappings->replacements[] = $description;
+$year = new stdClass();
+$year->from = "YEAR";
+$year->to = date("Y");//2018 -
+$mappings->replacements[] = $year;
+$templates = new stdClass();
+$templates->from = $tmpDir;
+$templates->to = $templatesPath;
+$mappings->replacements[] = $templates;
+
 
 // iterate over all your templates
-foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($tplDir), RecursiveIteratorIterator::LEAVES_ONLY) as $file)
+foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($tplDir), RecursiveIteratorIterator::LEAVES_ONLY) as $tmpl)
 {
     // force compilation
-    if ($file->isFile()) {
-        $twig->loadTemplate(str_replace($tplDir.'/', '', $file));
+    if ($tmpl->isFile()) {
+        $short_name = str_replace($tplDir.'/', '', $tmpl);
+        $template = $twig->loadTemplate($short_name);
+        $key = $cacheFS->generateKey($short_name, $twig->getTemplateClass($short_name) );
+        //echo $key."\r\n";
+        $cache_file = str_replace(
+            $tmpDir, '',
+            $key
+        );
+        $mappings->mappings[$cache_file] = new stdClass();
+        //$mappings->mappings[$cache_file]->key = $key;
+        $mappings->mappings[$cache_file]->fileName = "templates/".$short_name;
+        $mappings->mappings[$cache_file]->debugInfo = $template->getDebugInfo();
     }
+    $tmpl_file = 'templates/' . $tmpl;
 }
+file_put_contents(__DIR__."/../tmp/mapping.json",json_encode($mappings, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 exec(
 'xgettext --force-po --from-code=UTF-8'.
 ' --default-domain=account-manager'.
 ' --copyright-holder="William Desportes"'.
-' --package-name="wdes/account-manager 1.0.0-alpha1"'.
+' --package-name="wdes/account-manager"'.
+' --package-version="1.0.0-alpha1"'.
 ' --msgid-bugs-address=williamdes@wdes.fr'.
+' --from-code=utf-8'.
+' --keyword=__ --keyword=_gettext --keyword=_pgettext:1c,2 --keyword=_ngettext:1,2'.
 ' -p ./locale'.
 ' --from-code=UTF-8'.
 ' --add-comments=l10n'.
-' --add-location -L PHP $(find $tmpDir -name "*.php") -o '.__DIR__."/../po/account-manager.pot");
+' --add-location -L PHP $(find "$(pwd -P)" \( -name "*.php" \) -not -path "$(pwd -P)/vendor/*" | sort) -o '.__DIR__."/../po/account-manager.pot");
+
--- scripts/po_update.php
+++ scripts/po_update.php
@@ -1,25 +1,49 @@
 #!/usr/bin/php
 <?php
-
-exec("export LC_ALL=C");
+/**
+ * @license http://unlicense.org/UNLICENSE The UNLICENSE
+ * @author William Desportes <williamdes@wdes.fr>
+ */
 $po_file = realpath(__DIR__."/../po/account-manager.pot");
+$mappings = new stdClass();
+$mappings->mappings = array();
+$mappings->replacements = array();
+
+if (is_file(__DIR__."/../tmp/mapping.json"))
+    $mappings = json_decode(file_get_contents(__DIR__."/../tmp/mapping.json"));
 
 function poupdate($po_file) {
+    global $mappings;
     $pot_contents = file_get_contents($po_file);
-    $pot_contents = str_replace("SOME DESCRIPTIVE TITLE", "Wdes Account manager translation", $pot_contents);
-    $pot_contents = str_replace("YEAR", "(C) 2018 - ".date("Y"), $pot_contents);
+    foreach($mappings->replacements as $replacement ) {
+        $pot_contents = str_replace($replacement->from, $replacement->to, $pot_contents);
+    }
+    // Replace filename by name
+    $pot_contents = preg_replace_callback(
+        '@([0-9a-f]{2}\/[0-9a-f]*.php):([0-9]*)@',
+        function ($matchs) {
+            global $mappings;
+            $line = intval($matchs[2]);
+            $replace = $mappings->mappings->{$matchs[1]};
+            foreach ($replace->debugInfo as $cacheLineNumber => $iii) {
+                if ($line >= $cacheLineNumber) {
+                    return $replace->fileName . ':' . $iii;
+                }
+            }
+            return $replace->fileName . ':0';
+        },
+        $pot_contents
+    );
     file_put_contents($po_file, $pot_contents);
 }
+
+poupdate($po_file);
+
 $podir = realpath(__DIR__."/../po/")."/";
 echo "PoDir: ${podir}\r\n";
 foreach (glob("${podir}*.po") as $file) {
-    exec("msgmerge --previous -U $file ${podir}account-manager.pot");
+    exec("msgmerge --quiet --previous -U $file ${podir}account-manager.pot");
     echo "File: $file\r\n";
     poupdate($file);
 }
 
-/*
-# Create commit
-git add "po/*.po" po/phpmyadmin.pot
-git commit -s -m '[PO] Update files' -m '[CI skip]'
-*/
