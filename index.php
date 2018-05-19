4c5360a7632387a0cc18e582fe94b84b3b07de54
William Desportes <williamdes@wdes.fr>
2018-05-19 16:00:34 +0200

    Added assets

--- index.php
+++ index.php
@@ -12,6 +12,11 @@ $twig = new Twig_Environment($loader, array(
     'cache' => __DIR__.'/tmp/twig',
     'debug' => true
 ));
+$twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) {
+    // implement whatever logic you need to determine the asset path
+
+    return sprintf('public/assets/%s', ltrim($asset, '/'));
+}));
 
 $twig->addExtension(new AccountManager\Twig\I18nExtension());
 
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const vendorroot = path.resolve("./vendor/");
+const publicroot = path.resolve("./public/");
+module.exports = {
+    entry: publicroot + "/assets/js/index.js",
+    output: {
+		path: publicroot + "/assets/js",
+		filename: "bundle.js"
+	},
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: vendorroot+'/twbs/bootstrap/dist/css/*.*', to: publicroot+'/assets/css/[name].[ext]' },
+            { from: vendorroot+'/twbs/bootstrap/dist/js/*.*', to: publicroot+'/assets/js/[name].[ext]' }
+          ], { copyUnmodified: true }),
+    ]
+};
