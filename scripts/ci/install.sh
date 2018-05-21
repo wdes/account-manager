16f7b5c2a9b9506ebd31885951f5a1ed63e6edc4
William Desportes <williamdes@wdes.fr>
2018-05-21 15:41:07 +0200

    Typo fix

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -3,10 +3,10 @@ ME=$(dirname $0)
 export BDD_PWD=$(openssl rand -base64 100)
 export PATH=~/.composer/vendor/bin/:$PATH
 mysql -uroot -e "CREATE DATABASE IF NOT EXISTS accountmanager"
-mysql -uroot < sql/structure.sql
-mysql -uroot < sql/routines.sql
-mysql -uroot < sql/views.sql
-mysql -uroot < sql/events.sql
-mysql -uroot < sql/triggers.sql
+mysql -uroot < $ME/../../sql/structure.sql
+mysql -uroot < $ME/../../sql/routines.sql
+mysql -uroot < $ME/../../sql/views.sql
+mysql -uroot < $ME/../../sql/events.sql
+mysql -uroot < $ME/../../sql/triggers.sql
 mysql -uroot -e "SET PASSWORD = PASSWORD('$BDD_PWD')"
 echo -e "DB_HOST=localhost\r\nDB_USER=root\r\nDB_PASS=$BDD_PWD\r\n" > $ME/../../.env
