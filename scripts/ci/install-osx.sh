ae297b95a56168a3c4e92311382a7469dd7eb6fa
William Desportes <williamdes@wdes.fr>
2018-06-15 14:09:09 +0200

    Removed unused code
    [CI skip]

--- scripts/ci/install-osx.sh
+++ scripts/ci/install-osx.sh
@@ -13,8 +13,7 @@ rm /etc/my.cnf 2>/dev/null
 mysql.server start
 ls /tmp
 
-brew install php72 gettext
-brew link gettext --force
+brew install php72
 sed -i -e 's/^memory_limit = .*/memory_limit = -1/' /usr/local/etc/php/7.2/php.ini
 curl https://getcomposer.org/installer | php
 ln -s "$(pwd)/composer.phar" /usr/local/bin/composer
