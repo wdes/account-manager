d3d13cd801020c12f198c921d84b384c3a1b3c8e
William Desportes <williamdes@wdes.fr>
2018-06-19 02:42:35 +0200

    [CI] Try to fix osx

--- scripts/ci/install-osx.sh
+++ scripts/ci/install-osx.sh
@@ -14,6 +14,10 @@ mysql.server start
 ls /tmp
 
 brew install php72
+brew reinstall gettext
+brew link gettext --force
+export PATH="/usr/local/opt/gettext/bin:$PATH"
+echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
 sed -i -e 's/^memory_limit = .*/memory_limit = -1/' /usr/local/etc/php/7.2/php.ini
 curl https://getcomposer.org/installer | php
 ln -s "$(pwd)/composer.phar" /usr/local/bin/composer
